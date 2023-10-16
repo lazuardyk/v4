0a541e3efdbbdb7656b69e0f059c2d67b083c92a
lazuardyk <dimazkid@gmail.com>
2023-10-16 18:09:25 +0700

    ⚙️chore(file-renaming) Update image file name 🛠️refactor(content) Update personal details and experience 🚀feat(skills) Add new skills

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['JavaScript (ES6+)', 'TypeScript', 'Puppeteer', 'Node.js', 'Selenium', 'Python'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +135,22 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Lazuardy and I enjoy creating things that live on the internet. My
+              interest in web development started back in 2018 when I decided to try creating a
+              simple page to bypass ads for downloadable content — it taught me a lot about web
+              development & automation processes.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              Fast-forward to today, I have working on{' '}
+              <a href="https://www.upwork.com/freelancers/~017f3a1ca81d5e1191">Upwork</a> platform
+              as a freelancer and I’ve had the privilege of working with worldwide clients such as{' '}
+              <a href="https://www.chegg.com/">Chegg</a>,{' '}
+              <a href="https://www.woflow.com/">Woflow</a>,{' '}
+              <a href="https://www.needles.io/">Needles</a>,{' '}
+              <a href="https://booksy.com/">Booksy</a>, and many more. My main focus these days is
+              building automation products at <a href="https://disrupt.social/">Disrupt Social</a>{' '}
+              under the Upwork contract.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
@@ -173,7 +165,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpg"
+              src="../../images/me.png"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
